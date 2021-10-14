94d3243fb5c2b93e9ce5a641f08316666d33aa88
seonjangwon <wkddnjs740@naver.com>
2021-10-14 18:14:21 +0900

    20211014home

--- JavaClass/src/day07_20211013/Ex07_ArrayCoin.java
+++ JavaClass/src/day07_20211013/Ex07_ArrayCoin.java
@@ -12,22 +12,28 @@ public class Ex07_ArrayCoin {
 
 		int[] coin = { 500, 100, 50, 10 };
 		int num1 = 0; // 거스름 돈
-		int num2 = 0; // 남은돈
-		
+		int num2 = 0; // 동전 숫자
+
 		System.out.print("거스름 돈 : ");
 		num1 = scan.nextInt();
 
 		for (int i = 0; i < coin.length; i++) {
-			if (num1 >= coin[i]) {
-				num2 = num1 / coin[i];
-				num1 -= (num2*coin[i]);
+			if (num1 >= coin[i]) { // if를 쓰면 필요 없는 동전은 안나옴
+				num2 = num1 / coin[i]; // 필요 동전  숫자
+				num1 -= (num2*coin[i]); // 거스름돈에서 잔돈(동전 x 숫자)을 빼자
 				System.out.println(coin[i]+"원 "+num2+"개");
 				System.out.println(num1+"원 남음");
 			}
-			scan.close();
 		}
 		
+//		for (int i = 0; i < coin.length; i++) {
+//			num2 = num1 / coin[i];
+//			System.out.println(coin[i] + "원 " + num2 + "개");
+//			num1 = num1 % coin[i];
+//			num1%=num2;
+//		}
 
+		scan.close();
 	}
 
 }
