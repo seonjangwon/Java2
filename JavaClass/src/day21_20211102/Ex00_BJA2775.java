ceec7eb564e6c115120b1c12633845dbc6ff5928
seonjangwon <wkddnjs740@naver.com>
2021-11-02 16:52:18 +0900

    20211102_a1

--- JavaClass/src/day21_20211102/Ex00_BJA2775.java
+++ JavaClass/src/day21_20211102/Ex00_BJA2775.java
@@ -1,10 +1,11 @@
-package day19_20211029;
+package day21_20211102;
 
 import java.util.Scanner;
 
 public class Ex00_BJA2775 {
 
 	public static void main(String[] args) {
+		// 사실 깃 푸시용으로 만듬
 		// 백준알고리즘 2775
 		// 집이 이상해 a층 b호에 사려면 아래층(a-1)의 b호까지의 인구합의 만큼 살아야해
 		// 아파트에 0층도 있어
@@ -21,6 +22,7 @@ public class Ex00_BJA2775 {
 		int floor = 0; // k
 		int num = 0; // n
 		int result2 = 0;
+		int result3 = 0;
 		test = sc.nextInt();
 
 		for (int t = 0; t < test; t++) {
@@ -29,6 +31,7 @@ public class Ex00_BJA2775 {
 
 			for (int j = 1; j <= floor; j++) {
 				int result1 = 0;
+				result2 = 0;
 				for (int i = 1; i <= num; i++) {
 					result1 += i;
 					System.out.println("i1 : " + result1);
@@ -36,10 +39,11 @@ public class Ex00_BJA2775 {
 				System.out.println("j1 : " + result1);
 				result2 += result1;
 				System.out.println("j2 : " + result2);
+				result3= result2;
 			}
-
+			
 		}
-		System.out.println("h : " + result2);
+		System.out.println("h : " + result3);
 	}
 
 }
